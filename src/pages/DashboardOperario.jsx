6654782f83448002e1e767f89043d42afddd5497
alejandro2202-bit <luisalejandroguerrero902@gmail.com>
2026-09-24 11:28:36 -0500

    style: format DashboardOperario.jsx

--- src/pages/DashboardOperario.jsx
+++ src/pages/DashboardOperario.jsx
@@ -1,1 +1,12 @@
-import React from"react";import RutaProtegida from"../components/autenticacion/RutaProtegida";import DashboardOperarioContenido from"../components/dashboard/DashboardOperarioContenido";export default function DashboardOperario(){return <RutaProtegida><DashboardOperarioContenido/></RutaProtegida>}
+import React from "react";
+
+import RutaProtegida from "../components/autenticacion/RutaProtegida";
+import DashboardOperarioContenido from "../components/dashboard/DashboardOperarioContenido";
+
+export default function DashboardOperario() {
+  return (
+    <RutaProtegida>
+      <DashboardOperarioContenido />
+    </RutaProtegida>
+  );
+}
