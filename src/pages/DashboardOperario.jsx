3e1fe3d0b4b347618f8ba615ade4d7fc0f70012b
alejandro2202-bit <luisalejandroguerrero902@gmail.com>
2026-09-24 11:20:03 -0500

    feat: build operario dashboard and settings

---
+++ src/pages/DashboardOperario.jsx
@@ -0,0 +1,1 @@
+import React from"react";import RutaProtegida from"../components/autenticacion/RutaProtegida";import DashboardOperarioContenido from"../components/dashboard/DashboardOperarioContenido";export default function DashboardOperario(){return <RutaProtegida><DashboardOperarioContenido/></RutaProtegida>}
